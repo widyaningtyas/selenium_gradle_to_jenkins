270bf56a8e5a6d6e00cf70df29b4a86a41cfcaef
winda.widyaningtyas <winda.widyaningtyas@krom.id>
2025-02-24 12:41:13 +0700

    add npm path

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,3 +15,17 @@ dependencies {
     implementation("io.cucumber:cucumber-java:7.15.0")
     implementation("io.cucumber:cucumber-junit:7.15.0")
 }
+
+tasks.register<JavaExec>("gradle-test") {
+    dependsOn("assemble", "compileTestJava")
+    mainClass.set("io.cucumber.core.cli.Main")
+    classpath = sourceSets["test"].runtimeClasspath
+
+    val defaultArgs = listOf(
+            "--glue", "stepdefinitions",
+            "--plugin", "pretty",
+            "--plugin", "json:cucumber-html-reports/cucumber.json",
+            "--tags", "@Regression"
+    )
+    args = defaultArgs
+}
